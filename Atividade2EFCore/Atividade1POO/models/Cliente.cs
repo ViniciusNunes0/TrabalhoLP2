0c7187fd0bd0ed614014e0d6c3cd3090d35a7f39
ViniciusNunes0 <viniciusnb14@gmail.com>
2019-02-04 08:56:31 -0300

    banco.db

---
+++ Atividade2EFCore/Atividade1POO/models/Cliente.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class Cliente
+{
+    public string nome { get; set; }
+
+    [Key]
+    public int idCliente { get; set; }
+    
+}
---
+++ Atividade2EFCore/Atividade1POO/models/ContaCorrente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+public class ContaCorrente
+{
+    public const decimal Taxa = 0.10M;
+    public string titular = string.Empty;
+    public decimal Saldo { get; set; }
+
+    public ContaCorrente(string t)
+    {
+        this.titular = t;
+    }
+    public ContaCorrente()
+    {
+
+    }
+    [Key]
+    public int Id
+    {
+        get; set;
+    }
+
+    public void Depositar(decimal valor)
+    {
+        decimal desconto = valor * Taxa;
+        Saldo += valor;
+    }
+
+    public void Sacar(decimal valor)
+    {
+        decimal desconto = valor * Taxa;
+        if(valor <= Saldo)
+        {
+            Saldo -= valor;
+        }
+    }
+
+    public string Titular
+    {
+        get { return titular; }
+        set { titular = value; }
+    }
+        
+}
